cc797ebfea23e248eb4929adbf4dd56a240289da
Emu236 <sebastian.musial.sm@gmail.com>
2025-11-27 12:03:40 +0100

    Naprawa adresu IP dla Rendera

--- server/server.js
+++ server/server.js
@@ -53,6 +53,6 @@ app.post('/api/image', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Serwer działa na porcie ${PORT}`);
 });
